a68adabbb0cd27a7e6da4fd4835cb8834f173ed4
gitaeks <break@kimsq.com>
2015-10-28 15:01:53 +0900

    초기화 작업

--- lang.korean/admin/_pc/var/var.menu.php
+++
@@ -1,8 +0,0 @@
-<?php
-$d['amenu']['config'] = '블로그 공통 운영환경 설정';
-$d['amenu']['main'] = '블로그 리스트 &amp; 개별환경 설정';
-$d['amenu']['post'] = '포스트 관리';
-$d['amenu']['comment'] = '댓글 관리';
-$d['amenu']['upload'] = '첨부파일 관리';
-$d['amenu']['mig'] = '마이그레이션';
-?>
